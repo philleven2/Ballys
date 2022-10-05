7228df3479b3632a2e06cced9dcf2f2a37fec7e9
phil <pleven7511@charter.net>
2022-10-05 17:42:59 -0400

    First commit.

---
+++ src/Ballys/random/number/GenerateRandomNumbers.java
@@ -0,0 +1,44 @@
+package Ballys.random.number;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.PrimitiveIterator;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class GenerateRandomNumbers {
+
+	
+	public static void main (String[] args) {
+			
+			try (FileWriter writer = new FileWriter("randomNumbers.txt");) {
+				
+				// Generate 1,000,000 random numbers
+				IntStream nbrStream = generateStream(1000000, 1, 2000000).sorted();
+				
+				// Create IntStream iterator
+				PrimitiveIterator.OfInt itr = nbrStream.iterator();
+				
+				// Write IntStream to .txt file
+				while (itr.hasNext()) {
+					
+					writer.write(itr.nextInt() + "\t" + "");
+		           
+		        }
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+
+			}
+		
+	}
+	
+	private static IntStream generateStream(int size, int min, int max) {
+		
+		Random random = new Random();
+		return random.ints(size, min, max).sorted();
+        
+	}
+	
+}
